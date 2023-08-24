45bb214d391e2437ccf962cf7c3bbf51cba72537
David Lopez <davidnoelopez@gmail.com>
2023-08-23 23:17:23 -0600

    moved layout file

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Navbar from "./Navbar";
-import Header from "./Header";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 
 type LayoutProps = {
   children: React.ReactNode;
