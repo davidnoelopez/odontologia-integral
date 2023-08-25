ecaa252abd96cf7f5f10411d96778b4ca9300eb4
David Lopez <davidnoelopez@gmail.com>
2023-08-24 19:53:01 -0600

    added mobil banner

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div className="relative block w-full">
-      <div className="inset-0 -z-10">
+      <div className="inset-0 -z-10 hidden sm:block">
         <Image
           className="w-full rounded-3xl"
           src="/banner-1.png"
@@ -13,9 +13,19 @@ const Hero = () => {
           quality={100}
         />
       </div>
-      <div className="absolute inset-0 grid h-auto w-full grid-cols-2 place-content-center bg-transparent">
-        <div className="ml-8 flex flex-col items-center justify-center gap-4">
-          <div className="flex max-w-sm flex-col items-start gap-2 rounded-md p-2 backdrop-blur-sm">
+      <div className="inset-0 -z-10 block sm:hidden">
+        <Image
+          className="w-full rounded-3xl"
+          src="/mobil-banner.png"
+          width={920}
+          height={484}
+          alt="Banner Odontologia Integral"
+          quality={100}
+        />
+      </div>
+      <div className="absolute inset-0 grid h-auto w-full grid-cols-1 place-content-center bg-transparent sm:grid-cols-2">
+        <div className="mx-6 flex flex-col items-center justify-center gap-4 sm:mx-0 sm:ml-8">
+          <div className="flex max-w-sm flex-col gap-2 rounded-md p-2 backdrop-blur-sm">
             <h2 className="text-sm font-bold text-[#1370B5]">
               Bienvenido a nuestro consultorio digital
             </h2>
@@ -27,10 +37,10 @@ const Hero = () => {
               cada momento se exprese sin malos entendidos
             </p>
             <div className="flex w-full items-center justify-between space-x-6 text-sm font-semibold">
-              <button className="w-full rounded-lg bg-[#1370B5] px-4 py-2 text-white shadow-lg hover:bg-[#2c5d82]">
+              <button className="rounded-lg bg-[#1370B5] px-4 py-2 text-white shadow-lg hover:bg-[#2c5d82] sm:w-full">
                 Acerca de nosotros
               </button>
-              <button className="w-full rounded-lg bg-white px-4 py-2 text-[#1370B5] shadow-lg hover:bg-slate-200">
+              <button className="rounded-lg bg-white px-4 py-2 text-[#1370B5] shadow-lg hover:bg-slate-200 sm:w-full">
                 Tratamientos
               </button>
             </div>
